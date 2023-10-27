8dea4ec8a7dca51e3963e87e9da2f0b9f178849a
Aus02 <ausmitagaro@gmail.com>
2023-10-27 06:52:55 +0530

    changes to post request with middleware and accept array in request

--- src/routes/aiBot.js
+++ src/routes/aiBot.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const {OpenAiChat}=require("../controllers/aiBotController");
+const myAuth=require("../middlewares/auth");
 const aiRouter=express.Router();
 const dotenv = require('dotenv');
 
